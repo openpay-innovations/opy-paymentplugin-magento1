fa2b8f581a467d145a7c146e68bedc3f39eae319
elisabethSchacht <elisabeth.s@ideatarmac.com>
2021-10-16 04:52:28 +0200

    our latest version (#5)
    * our latest version
    
    * issue with total amount

--- app/code/community/Openpay/Payment/Model/paymentadapter/businessLayer/Magento/Shopsystem.php
+++ app/code/community/Openpay/Payment/Model/paymentadapter/businessLayer/Magento/Shopsystem.php
@@ -34,7 +34,7 @@ class Shopsystem
         }
         $shopCart->total = $cart->getGrandTotal();
         $integerTotal = round((float)$cart->getGrandTotal(), 2);
-        $shopCart->integerTotal = ($integerTotal * 100);
+        $shopCart->integerTotal =(int)round($integerTotal * 100);
         $shopCart->deliveryAddress = $cart->getShippingAddress()->getData();
         $shopCart->invoiceAddress = $cart->getBillingAddress()->getData();
 
@@ -44,20 +44,48 @@ class Shopsystem
         $linesDelivery = explode("\n", $shopCart->deliveryAddress['street']);
         $shopCart->deliveryAddress['line1'] = $linesDelivery[0];
         $shopCart->deliveryAddress['line2'] = (isset($linesDelivery[1])) ? $linesDelivery[1] : '';     
-        
-        if (!empty($shopCart->deliveryAddress['region'])) {
-            $regionDelivery = \Mage::getModel('directory/region')
-                ->loadByName($shopCart->deliveryAddress['region'], $shopCart->deliveryAddress['country_id'])
-                ->getData();
-            $shopCart->deliveryAddress['regioncode'] = (isset($regionDelivery['code']) && $regionDelivery['code']) ? $regionDelivery['code'] : $shopCart->deliveryAddress['region'];
+        $deliveryAddress = $shopCart->deliveryAddress;
+        if ($deliveryAddress['street'] == null && 
+            $deliveryAddress['city'] == null &&
+            $deliveryAddress['region'] == null &&
+            $deliveryAddress['postcode'] == null
+        ) {
+            $shopCart->deliveryMethod = 'Email';
+        } else {
+            $deliveryAddress['city'] = empty($deliveryAddress['city']) ? '-' : $deliveryAddress['city'];
+            $deliveryAddress['postcode'] = empty($deliveryAddress['postcode']) ? '-' : $deliveryAddress['postcode'];
+            if (!empty($shopCart->deliveryAddress['region'])) {
+                $regionDelivery = \Mage::getModel('directory/region')
+                    ->loadByName($shopCart->deliveryAddress['region'], $shopCart->deliveryAddress['country_id'])
+                    ->getData();
+                $shopCart->deliveryAddress['regioncode'] = (isset($regionDelivery['code'])) ? $regionDelivery['code'] : $shopCart->deliveryAddress['region'];
+            } else {
+                $shopCart->deliveryAddress['regioncode'] = '-';
+            }
+            if (!empty($shopCart->deliveryAddress['street'])) {
+                $linesDelivery = explode("\n", $shopCart->deliveryAddress['street']);
+                $shopCart->deliveryAddress['line1'] = $linesDelivery[0];
+                $linesDelivery2 = (isset($linesDelivery[1])) ? $linesDelivery[1] : '';
+                if (isset($linesDelivery[2])) {
+                    $linesDelivery2 .= ' ' . $linesDelivery[2];
+                }
+                $shopCart->deliveryAddress['line2'] = $linesDelivery2;
+            } else {
+                $shopCart->deliveryAddress['line1'] = '-';
+            }
+         }
+
+        if (!empty($shopCart->invoiceAddress['firstname']) && !empty($shopCart->invoiceAddress['lastname'])) {
+                 $shopCart->deliveryAddress['firstname'] = $shopCart->invoiceAddress['firstname'] ;
+                 $shopCart->deliveryAddress['lastname'] = $shopCart->invoiceAddress['lastname'] ;
         }
+
         if (!empty($shopCart->invoiceAddress['region'])) {
             $regionInvoice = \Mage::getModel('directory/region')
                 ->loadByName($shopCart->invoiceAddress['region'], $shopCart->invoiceAddress['country_id'])
                 ->getData();
             $shopCart->invoiceAddress['regioncode'] = (isset($regionInvoice['code']) && $regionInvoice['code']) ? $regionInvoice['code'] : $shopCart->invoiceAddress['region']; 
-        }
-        
+        }    
         $shopCart->isoCurrency = \Mage::app()->getStore()->getCurrentCurrencyCode();
         $shopCart->cartId = $cart->getId();
         $shopCart->source = 'Magento';
